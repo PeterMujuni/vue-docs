1a9a146c069d84a9ed72d8a901e8625814192688
Peter Mujuni Mwebesa <pmm@chronoshub.io>
2023-05-03 08:33:42 +0200

    computed props

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="increment">{{ text }} {{ count }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: "ButtonComponent",
+        props: {
+            text: String,
+        },
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.count++
+            }
+        }
+    })
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,97 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Hello View :)</h1>
+    <div v-if="showBook">
+    <ul>
+      <li @click="toggleIsFav(book)" v-for="book in books" :key="book.title" :class="{ fav: book.isFav}">
+        <img :src="book.img" :alt="book.title" />
+        <h3>{{ book.title }}</h3>
+      </li>
+    </ul>
+      
+    </div>
+    <button @click="toggleShowBox">
+      <span v-if="showBook">Hide Books</span>
+      <span v-else>Show Books</span>
+    </button>
+    <br/>
+
+    <!-- mouse events -->
+    <!-- <div class="box" @mouseover="handleEvent($event, 5)">mouseover</div>
+    <div class="box" @mouseleave="handleEvent">mouseleave</div>
+    <div class="box" @dblclick="handleEvent">dblclick</div>
+    <div class="box" @mousemove="handleMousemove">position - {{ x }} {{ y }}</div> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      showBook: true,
+      books: [
+        {title: "The Name og the Wind", author: "Brandon Sanderson", img: require('@/assets/1.jpg'), isFav: true},
+        {title: "The Final Dynasti", author: "Neo Anderson", img: require('@/assets/2.jpg'), isFav: false},
+        {title: "The Final Empire", author: "Peter Anderson", img: require('@/assets/3.jpg'), isFav: true}
+      ],
+      x: 0,
+      y: 0,
+    }
   },
+  methods: {
+    toggleShowBox() {
+      this.showBook = !this.showBook
+    },
+    toggleIsFav(book: any) {
+      book.isFav = !book.isFav
+    }
+  },
+  // computed: {
+  //   filteretBooks() {
+  //     return this.books.filter((book: any) => book.isFav)
+  //   } 
+  // }
 });
 </script>
+
+<style lang="scss">
+  body {
+    background-color: #eee;
+    max-width: 960px;
+    margin: 20px auto;
+  }
+</style>
+
+<style scoped lang="scss">
+
+  p, h3, ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    list-style-type: none;
+    background-color: #fff;
+    margin: 20px auto;
+    padding: 10px 20px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &.fav {
+      background-color: #ff9ed2;
+      color: white;
+    }
+  }
+
+  .box {
+    padding: 100px;
+    width: 400px;
+    text-align: center;
+    background-color: #ddd;
+    margin: 20px;
+    display: inline-block;
+  }
+</style>
